937519115c4b31fb2b837f74aef357e880b9e533
AguiarD <douglasv.aguiar@gmail.com>
2023-02-20 17:20:12 -0300

    Douglas_Controller_Cliente

--- AceleraPleno.API/Controllers/ClienteController.cs
+++ AceleraPleno.API/Controllers/ClienteController.cs
@@ -1,6 +1,7 @@
 ﻿using AceleraPleno.API.Interface;
 using AceleraPleno.API.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AceleraPleno.API.Controllers
@@ -16,7 +17,7 @@ namespace AceleraPleno.API.Controllers
             _iRepository = iRepository;
         }
 
-        [HttpGet]
+        [HttpGet, Route("Listar")]
         public async Task<IActionResult> Listar()
         {
             if (!ModelState.IsValid)
@@ -29,11 +30,55 @@ namespace AceleraPleno.API.Controllers
             return Ok(clientes);
         }
 
-        [HttpPost]
+        [HttpPost, Route("Incluir")]
         public async Task<IActionResult> Incluir(Cliente cliente)
         {
            await  _iRepository.Adicionar(cliente);
             return Ok(cliente);
         }
+
+        [HttpGet, Route("FiltrarPoId")]
+        public async Task<IActionResult> FiltrarPorId(Guid id)
+        {
+            var cliente = await _iRepository.FiltrarId(id);
+            return Ok(cliente);
+        }
+
+        [HttpPut, Route("Atualizar")]
+        public async Task<IActionResult> Atualizar(Cliente cliente)
+        {
+            if (cliente == null)
+                return BadRequest();
+            var clienteAtualizado = await _iRepository.Atualizar(cliente, cliente.Id);
+            return Ok(clienteAtualizado);
+        }
+
+        [HttpDelete, Route("Deletar")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            if (!await _iRepository.Excluir(id))
+                return BadRequest();
+
+            var clientes = await _iRepository.Listar();
+            return Ok(clientes);
+        }
+
+        /*[HttpPatch]
+        public async Task<IActionResult> AtualizarNome(Guid id, [FromBody] JsonPatchDocument<Cliente> clienteParcial)
+        {
+            if(clienteParcial == null)
+                return BadRequest();
+
+            var clienteAtual = await _iRepository.FiltrarId(id);
+
+            if (clienteAtual == null)
+                return NotFound();
+
+            clienteParcial.ApplyTo(clienteAtual, (Microsoft.AspNetCore.JsonPatch.Adapters.IObjectAdapter)ModelState);
+            var isValid = TryValidateModel(clienteAtual);
+
+            if (!isValid)
+                return BadRequest(ModelState);
+        }*/
     }
 }
